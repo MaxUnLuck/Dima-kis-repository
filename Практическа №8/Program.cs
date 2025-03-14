4b630239739a1da6a38c5ffa73cd3d65aa2527d3
MaxUnLuck <ggxuesos41@gmail.com>
2025-03-14 08:13:55 +0300

    Add files via upload

---
+++ Практическа №8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическа__8
+{
+    internal class Program
+    {
+            public static void Main(string[] args)
+            {
+            double sX = -5;
+            double eX = 5;
+            double step = 0.5;
+            Console.WriteLine("     x  |    y    ");
+            double x = sX;
+            while (x <= eX)
+            {
+                double y = 5 - (x * x) / 2;
+                Console.WriteLine($"  {x,5:F1} | {y,7:F2} ");
+                x += step;
+            }
+            Console.ReadKey();
+       }
+    }
+}
