aa47a870eb4515036a8582b5f88e8832a5d97c9a
MaxUnLuck <ggxuesos41@gmail.com>
2025-03-21 15:50:08 +0300

    Add files via upload

---
+++ Практическа 7.1/Input.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Практическа_7._1
+{
+    /// <summary>
+    /// Логика взаимодействия для Input.xaml
+    /// </summary>
+    public partial class Input : Window
+    {
+        public Input()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
